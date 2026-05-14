dcc3d6a0039deac818c22b9dc38e453df51b0888
DungDuong <dungduong08062003@gmail.com>
2026-05-14 17:07:08 +0700

    change route/connectDB

--- routes/connectDB.py
+++ routes/connectDB.py
@@ -10,7 +10,19 @@ conn = psycopg2.connect(
 )
 
 cur = conn.cursor()
-
+def open_connection():
+    try:
+        conn = psycopg2.connect(
+            dbname="university_db",
+            user="dung",
+            password="matkhau_manh",
+            host="localhost",
+            port="5432"
+        )
+        return conn
+    except Exception as e:
+        print(f"Error connecting to database: {e}")
+        return None
 # Thực hiện truy vấn
 cur.execute("SELECT * FROM university;")
 rows = cur.fetchall()
---
+++ routes/university.py
@@ -0,0 +1,56 @@
+import models.university as university_model
+import connectDB as db
+def get_university_info():
+    # Tạo đối tượng university_data
+    conn = db.open_connection()
+    if not conn:
+        return {"error": "Failed to connect to database"}
+
+    sql = "SELECT * FROM university;"
+    try:
+        cur = conn.cursor()
+        cur.execute(sql)
+        rows = cur.fetchall()
+        if rows:
+            university_info = university_model.university_data(
+                ID_university=rows[0][0],
+                new_data_description=rows[0][1],
+                url_info=rows[0][2]
+            )
+            return university_info.to_dict()
+        else:
+            return {"error": "University not found"}
+    except Exception as e:
+        print(f"Error fetching university info: {e}")
+        return {"error": "Failed to fetch university info"}
+    finally:
+        if conn:
+            conn.close()
+def get_list_industry_info_university(university_id):
+    conn = db.open_connection()
+    if not conn:
+        return {"error": "Failed to connect to database"}
+
+    sql = "SELECT * from university_data where ID_university = %s"
+    try:
+        cur = conn.cursor()
+        cur.execute(sql, (university_id,))
+        rows = cur.fetchall()
+        if rows:
+            industry_info_list = []
+            for row in rows:
+                industry_info = university_model.industry_data(
+                    ID_industry=row[0],
+                    name_industry=row[1],
+                    description_industry=row[2]
+                )
+                industry_info_list.append(industry_info.to_dict())
+            return industry_info_list
+        else:
+            return {"error": "No industry information found for the specified university"}
+    except Exception as e:
+        print(f"Error fetching industry information: {e}")
+        return {"error": "Failed to fetch industry information"}
+    finally:
+        if conn:
+            conn.close()
