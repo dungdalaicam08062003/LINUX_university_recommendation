5858ca37aa28b26dfa2eba7c7a5bd4fb8a2274c9
DungDuong <dungduong08062003@gmail.com>
2026-05-14 22:11:22 +0700

    change structure

--- communicate_Database/university.py
+++ communicate_Database/university.py
@@ -1,4 +1,5 @@
-import models.university as university_model
+import models.university as university
+import models.university_data as university_data
 import communicate_Database.connectDB as db
 def get_university_info():
     # Tạo đối tượng university_data
@@ -12,10 +13,10 @@ def get_university_info():
         cur.execute(sql)
         rows = cur.fetchall()
         if rows:
-            university_info = university_model.university(
-                ID_university=rows[0][0],
-                new_data_description=rows[0][1],
-                url_info=rows[0][2]
+            university_info = university.university(
+                name=rows[0][0],
+                location=rows[0][1],
+                url=rows[0][2]
             )
             return university_info.to_dict()
         else:
@@ -39,7 +40,7 @@ def get_list_industry_info_university(university_id):
         if rows:
             industry_info_list = []
             for row in rows:
-                industry_info = university_model.industry_data(
+                industry_info = university_data.industry_data(
                     ID_industry=row[0],
                     name_industry=row[1],
                     description_industry=row[2]
