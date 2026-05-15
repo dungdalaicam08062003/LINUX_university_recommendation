b888db9f0f8cfe2bfe39e2389fd844f776507b2d
DungDuong <dungduong08062003@gmail.com>
2026-05-15 14:19:37 +0700

    change structure

--- run_log.py
+++ run_log.py
@@ -39,7 +39,7 @@ class HttpLogHandler(logging.Handler):
     def emit(self, record):
         log_entry = self.format(record)
         try:
-            requests.post("http://192.168.207.133:5000/logs",
+            requests.post("http://100.68.174.55:5000/logs",
                           json={"log": log_entry}, timeout=2)
         except Exception as e:
             print("Failed to send log:", e)
