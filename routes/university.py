bedc7c230c177179d4b3f15e84cc7ec97a70cf2e
DungDuong <dungduong08062003@gmail.com>
2026-05-15 11:20:48 +0700

    change structure

--- routes/university.py
+++ routes/university.py
@@ -1,15 +1,18 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, jsonify, Response
 from communicate_Database import university
+import json
 
 
 bp = Blueprint("university", __name__, url_prefix="/university")
 
+
 @bp.route("/data", methods=["GET"])
 def get_university_data():
     university_info = university.get_university_info()
-    return jsonify(university_info)
-
-
+    return Response(
+        json.dumps(university_info, ensure_ascii=False),
+        content_type="application/json; charset=utf-8"
+    )
 # @bp.route("/upload", methods=["POST"])
 # def upload_sensor_data():
 #     payload = request.json
