ead71a5a3e1715acc33986f59fa409cab3321718
DungDuong <dungduong08062003@gmail.com>
2026-05-15 11:13:56 +0700

    change structure

--- models/university.py
+++ models/university.py
@@ -1,14 +1,14 @@
 class university:
-    def __init__(self, name, location, url):
+    def __init__(self, id, name, location, url):
+        self.id = id
         self.name = name
         self.location = location
         self.url = url
 
-    def get_info(self):
-        return f"{self.name} is located in {self.location} and can be visited at {self.url}."
     def to_dict(self):
         return {
-            'name': self.name,
-            'location': self.location,
-            'url': self.url
+            "id": self.id,
+            "name": self.name,
+            "location": self.location,
+            "url": self.url
         }
